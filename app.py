56a6f5f019d23bec70684915cb9d5027cfb579e3
kovashikawa <kovashikawaa@gmail.com>
2020-12-02 14:10:19 -0300

    test changes

--- app.py
+++ app.py
@@ -1,124 +1,230 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-
-import dash_cytoscape as cyto
-
-########### Define your variables
 import json
 import dash
-import urllib.request
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import dash_core_components as dcc
+
 import dash_cytoscape as cyto
+import dash_reusable_components as drc
 
 app = dash.Dash(__name__)
+app.config['suppress_callback_exceptions'] = True
 app.title = "Food Graphs"
 server = app.server
-tabtitle='Food from allrecipes.com!'
-mytitle='Recipes graph from allrecipes.com'
-myheading='Recipes\' Graph'
-githublink='https://github.com/kovashikawa/flying-dog-beers'
-sourceurl='https://www.allrecipes.com/'
 
 app.scripts.config.serve_locally = True
 app.css.config.serve_locally = True
 
-
 with open('./food.json', 'r') as f:
     data = json.loads(f.read())
-    
-#https://github.com/plotly/dash-cytoscape/blob/master/demos/data/edge-types/cy-style.json
-with open('cy-style.json') as f:
-    stylesheet = json.loads(f.read())
 
 styles = {
-    'container': {
-        'position': 'fixed',
-        'display': 'flex',
-        'flex-direction': 'column',
-        'height': '100%',
-        'width': '100%'
+    'json-output': {
+        'overflow-y': 'scroll',
+        'height': 'calc(50% - 25px)',
+        'border': 'thin lightgrey solid'
     },
-    'cy-container': {
-        'flex': '1',
-        'position': 'relative'
-    },
-    'cytoscape': {
-        'position': 'absolute',
-        'width': '100%',
-        'height': '100%',
-        'z-index': 999
-    }
+    'tab': {'height': 'calc(98vh - 80px)'}
 }
 
+default_stylesheet = [
+    {
+        "selector": 'node',
+        'style': {
+            "opacity": 0.65,
+        }
+    },
+    {
+        "selector": 'edge',
+        'style': {
+            "opacity": 0.65
+        }
+    }
+]
 
 # App
-app.layout = html.Div(style=styles['container'], children=[
-    html.Div([
-        html.Button("Responsive Toggle", id='toggle-button'),
-        html.Div(id='toggle-text')
-    ]),
-    html.Div(className='cy-container', style=styles['cy-container'], children=[
+app.layout = html.Div([
+    html.Div(className='eight columns', children=[
         cyto.Cytoscape(
             id='cytoscape',
             elements=data['elements'],
-            stylesheet=stylesheet,
-            style=styles['cytoscape'],
-            layout={
-                'name': 'cose',
-                'idealEdgeLength': 100,
-                'nodeOverlap': 20,
-                'refresh': 20,
-                'fit': True,
-                'padding': 30,
-                'randomize': False,
-                'componentSpacing': 100,
-                'nodeRepulsion': 400000,
-                'edgeElasticity': 100,
-                'nestingFactor': 5,
-                'gravity': 80,
-                'numIter': 1000,
-                'initialTemp': 200,
-                'coolingFactor': 0.95,
-                'minTemp': 1.0
-            },
-            responsive=True
+            style={
+                'height': '95vh',
+                'width': '100%'
+            }
         )
+    ]),
+
+    html.Div(className='four columns', children=[
+        dcc.Tabs(id='tabs', children=[
+            dcc.Tab(label='Control Panel', children=[
+                drc.NamedDropdown(
+                    name='Layout',
+                    id='dropdown-layout',
+                    options=drc.DropdownOptionsList(
+                        'random',
+                        'circle',
+                        'concentric',
+                        'cose'
+                    ),
+                    value='circle',
+                    clearable=False
+                ),
+
+                drc.NamedDropdown(
+                    name='Node Shape',
+                    id='dropdown-node-shape',
+                    value='ellipse',
+                    clearable=False,
+                    options=drc.DropdownOptionsList(
+                        'ellipse',
+                        'triangle',
+                        'rectangle',
+                        'diamond',
+                        'pentagon',
+                        'hexagon',
+                        'heptagon',
+                        'octagon',
+                        'star',
+                        'polygon',
+                    )
+                ),
+
+                drc.NamedInput(
+                    name='Followers Color',
+                    id='input_conn_color',
+                    type='text',
+                    value='#0074D9',
+                ),
+            ]),
+
+            dcc.Tab(label='JSON', children=[
+                html.Div(style=styles['tab'], children=[
+                    html.P('Node Object JSON:'),
+                    html.Pre(
+                        id='tap-node-json-output',
+                        style=styles['json-output']
+                    ),
+                    html.P('Edge Object JSON:'),
+                    html.Pre(
+                        id='tap-edge-json-output',
+                        style=styles['json-output']
+                    )
+                ])
+            ])
+        ]),
     ])
 ])
 
 
-@app.callback(Output('cytoscape', 'responsive'), [Input('toggle-button', 'n_clicks')])
-def toggle_responsive(n_clicks):
-    n_clicks = 2 if n_clicks is None else n_clicks
-    toggle_on = n_clicks % 2 == 0
-    return toggle_on
+@app.callback(Output('cytoscape-update-layout', 'layout'),
+              Input('dropdown-update-layout', 'value'))
+def update_layout(layout):
+    return {
+        'name': layout,
+        'animate': True
+    }
+
+@app.callback(Output('tap-node-json-output', 'children'),
+              [Input('cytoscape', 'tapNode')], suppress_callback_exceptions=False)
+def display_tap_node(data):
+    return json.dumps(data, indent=2)
 
 
-@app.callback(Output('toggle-text', 'children'), [Input('cytoscape', 'responsive')])
-def update_toggle_text(responsive):
-    return '\t' + 'Responsive ' + ('On' if responsive else 'Off')
+@app.callback(Output('tap-edge-json-output', 'children'),
+              [Input('cytoscape', 'tapEdge')])
+def display_tap_edge(data):
+    return json.dumps(data, indent=2)
 
 
-########### Initiate the app
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-# server = app.server
-# app.title=tabtitle
+@app.callback(Output('cytoscape', 'layout'),
+              [Input('dropdown-layout', 'value')])
+def update_cytoscape_layout(layout):
+    return {'name': layout}
 
-# ########### Set up the layout
-# app.layout = html.Div(children=[
-#     html.H1(myheading),
-#     dcc.Graph(
-#         id='allrecipes',
-#         figure='assets\\allrecipes_logo.png'
-#     ),
-#     html.A('Code on Github', href=githublink),
-#     html.Br(),
-#     html.A('Data Source', href=sourceurl),
-#     ]
-# )
+@app.callback(Output('cytoscape', 'stylesheet'),
+              [Input('cytoscape', 'tapNode'),
+               Input('input_conn_color', 'value'),
+               Input('dropdown-node-shape', 'value')])
 
-if __name__ == '__main__':
-    app.run_server()
+def generate_stylesheet(node, conn_color, node_shape):
+    if not node:
+        return default_stylesheet
+
+    stylesheet = [{
+        "selector": 'node',
+        'style': {
+            'opacity': 0.3,
+            'shape': node_shape
+        }
+    }, {
+        'selector': 'edge',
+        'style': {
+            'opacity': 0.2,
+            "curve-style": "bezier",
+        }
+    }, {
+        "selector": 'node[id = "{}"]'.format(node['data']['id']),
+        "style": {
+            'background-color': '#B10DC9',
+            "border-color": "purple",
+            "border-width": 2,
+            "border-opacity": 1,
+            "opacity": 1,
+
+            "label": "data(value)",
+            "color": "#262347",
+            "text-opacity": 1,
+            "font-size": 48,
+            'z-index': 9999
+        }
+    }]
+
+    for edge in node['edgesData']:
+        if edge['source'] == node['data']['id']:
+            stylesheet.append({
+                "selector": 'node[id = "{}"]'.format(edge['target']),
+                "style": {
+                    'background-color': conn_color,
+                    'opacity': 0.9,
+
+                    "label": "data(value)",
+                    "color": "#03402e",
+                    "text-opacity": 0.7,
+                    "font-size": 36,
+                    'z-index': 9999
+                }
+            })
+            stylesheet.append({
+                "selector": 'edge[id= "{}"]'.format(edge['id']),
+                "style": {
+                    "line-color": conn_color,
+                    'opacity': 0.7,
+                    'z-index': 5000
+                }
+            })
+
+        if edge['target'] == node['data']['id']:
+            stylesheet.append({
+                "selector": 'node[id = "{}"]'.format(edge['source']),
+                "style": {
+                    'background-color': conn_color,
+                    'opacity': 0.7,
+                    'z-index': 9999,
+
+                    "label": "data(value)",
+                    "color": "#03402e",
+                    "text-opacity": 1,
+                    "font-size": 36
+                }
+            })
+            stylesheet.append({
+                "selector": 'edge[id= "{}"]'.format(edge['id']),
+                "style": {
+                    "line-color": conn_color,
+                    'opacity': 0.6,
+                    'z-index': 5000
+                }
+            })
+
+    return stylesheet
